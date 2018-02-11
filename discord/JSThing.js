0b8ae5e89a9ed5013194c9fe463b6ed85d5975a7
39hige1 <39hige1@gmail.com>
2018-02-11 21:08:52 +0100

    Delete JSThing.js

--- discord/JSThing.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-  $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-
-  $(window).resize(function(){
-    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-  });
-});
